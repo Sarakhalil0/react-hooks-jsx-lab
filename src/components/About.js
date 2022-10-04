f20736183fe5abb8a1ffb5b66a364f68ee7f9728
sara khalil <sarakhalil3377@gmail.com>
2022-10-04 18:04:55 +0300

    DONE

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+  <h2>About Me</h2>
+  <p>Anything you write in here will show on browser </p>
+  <img src={image} alt="I made this" />
+  </div>;
 }
 
 export default About;
